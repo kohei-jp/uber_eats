e5e12fbc991d11312bd435af132adaa3000aeeed
Kohei-jp <kohei.mitateda@gmail.com>
2021-01-10 20:47:47 +0900

    4.3~4.5 定数の定義、rails_apiをfetchする関数の定義

---
+++ frontend/src/apis/restaurants.js
@@ -0,0 +1,16 @@
+// フロントエンドからAPIを叩くための関数
+import axios from 'axios';
+import { restaurantsIndex } from '../urls/index'
+
+// railsのAPIを叩いて、JSON形式でデータが帰ってくる
+export const fetchRestaurants = async() => {
+  // get(uri)メソッドの基本形. POSTならaxios.post(...)
+  return await axios.get(restaurantsIndex)
+  .then(res => {
+    return res.data
+  })
+  // 本来は、varidationエラーを返す
+  .catch((e) => console.error(e))
+}
+
+// axiosはPromiseベース(Promiseを返す). newPromiseをしなくて良い
