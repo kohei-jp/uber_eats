9a3d0bb180b293caeaf8734b6a41ba73da839c60
Kohei-jp <kohei.mitateda@gmail.com>
2021-01-13 00:43:13 +0900

    5.1~5.6 restaurants.jsx作成

---
+++ frontend/src/reducers/restaurants.js
@@ -0,0 +1,32 @@
+//定数の読み込み
+import {REQUEST_STATE} from "../constants";
+
+//initialState
+export const initialState = {
+  fetchState: REQUEST_STATE.INITIAL, // GET_APIの状態
+  restaurantsList: [], // 取得したレストランの情報
+};
+
+//actionType
+export const restaurantsActionTypes = {
+  FETCHING: "FETCHING",
+  FETCH_SUCCESS: "FETCH_SUCCESS",
+}
+
+//reducer
+export const restaurantsReducer = (state, action) => {
+  switch (action.type) {
+    case restaurantsActionTypes.FETCHING:
+      return {
+        ...state,
+        fetchState: REQUEST_STATE.LOADING,
+      };
+    case restaurantsActionTypes.FETCH_SUCCESS:
+      return {
+        fetchState: REQUEST_STATE.OK,
+        restaurantsList: action.payload.restaurants,
+      };
+    default:
+      throw new Error();
+  }
+}
