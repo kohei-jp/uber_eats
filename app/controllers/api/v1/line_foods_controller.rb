b5d60559e5da29038e71c36e7449e35e4d29da55
Kohei-jp <kohei.mitateda@gmail.com>
2021-01-16 21:53:43 +0900

    リファクタの実施, 教材終了

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -6,15 +6,32 @@ module Api
       def index
         line_foods = LineFood.active.all
         if line_foods.exists?
-          render json: {
-            line_food_ids: line_foods.map{ |line_food| line_food.id },
-            restaurant: line_foods[0].restaurant,
-            count: line_foods.sum{ |line_food| line_food[:count] },
-            amount: line_foods.sum{ |line_food| line_food.total_amount },
+            line_food_ids = []
+            count = 0
+            amount = 0
+          
+            line_foods.each do |line_food|
+              line_food_ids << line_food.id # (1) idを参照して配列に追加する
+              count += line_food[:count] # (2)countのデータを合算する
+              amount += line_food.total_amount # (3)total_amountを合算する
+            end
+          
+            render json: {
+              line_food_ids: line_food_ids,
+              restaurant: line_foods[0].restaurant,
+              count: count,
+              amount: amount,
             }, status: :ok
-            # binding.pry
           else
           render json: {}, status: :no_content
+
+          # リファクタリング前(sumとmapを同じ変数に対して3回も行っている)
+          # render json: {
+          #   line_food_ids: line_foods.map{ |line_food| line_food.id },
+          #   restaurant: line_foods[0].restaurant,
+          #   count: line_foods.sum{ |line_food| line_food[:count] },
+          #   amount: line_foods.sum{ |line_food| line_food.total_amount },
+          #   }, status: :ok
         end
       end
 
--- db/migrate/20210108151111_create_foods.rb
+++ db/migrate/20210108151111_create_foods.rb
@@ -1,10 +1,10 @@
 class CreateFoods < ActiveRecord::Migration[6.0]
   def change
     create_table :foods do |t|
-      t.integer :restaurant_id, null: false
-      t.string :name, null: false
-      t.integer :price, null: false
-      t.text :description, null: false
+      t.integer :restaurant_id, null: false, comment: '所属するレストランのid'
+      t.string :name, null: false, comment: 'フードの名前'
+      t.integer :price, null: false, comment: 'フードの価格'
+      t.text :description, null: false, comment: 'フードの説明文'
 
       t.timestamps
     end
